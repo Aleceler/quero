5d179d1965da9f0a27da1db56ab8dcdfa9fecf24
alemaosin <aleceler@gmail.com>
2020-03-14 20:51:01 -0300

    Updates

---
+++ precache-manifest.004d9a0aa88a05acec614fd04806d8ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "588cccf1111be22e31b5b1ac14e23c8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c41e13a3984a0eb7b4e1",
+    "url": "/static/js/2.70bcdde8.chunk.js"
+  },
+  {
+    "revision": "59642121174bd7397964b34b2a9c4ebf",
+    "url": "/static/js/2.70bcdde8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7e81b6f19ce6656ff21",
+    "url": "/static/js/main.d35ebe35.chunk.js"
+  },
+  {
+    "revision": "61fddd75c3584d6ffc03",
+    "url": "/static/js/runtime-main.2c43a3f2.js"
+  },
+  {
+    "revision": "017704e7cd26a4437c75003d80d00ea8",
+    "url": "/static/media/quero.017704e7.svg"
+  }
+]);
